98d65714651317c8435424f84afae7a13f2b99a1
ANDYDEARING <andy.dearing@gmail.com>
2020-02-14 14:28:36 -0500

    working java build, needs data connection

--- API/src/main/java/com/chessapi/API/ApiApplication.java
+++ API/src/main/java/com/chessapi/API/ApiApplication.java
@@ -2,7 +2,7 @@ package com.chessapi.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// testing
+
 @SpringBootApplication
 public class ApiApplication {
 
